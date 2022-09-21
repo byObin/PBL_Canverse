a587d34a334f0a46c473412dc416caac586a7232
byObin <byobin26@gmail.com>
2022-09-21 16:48:03 +0900

    base project

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed = 5;
+
+
+    void Update()
+    {
+        float posX = Input.GetAxisRaw("Horizontal");    //keybord input
+        float posY = Input.GetAxisRaw("Vertical");         //keybord input
+
+
+        transform.Translate(new Vector2(posX, posY) * Time.deltaTime * speed);
+
+
+    }
+}
