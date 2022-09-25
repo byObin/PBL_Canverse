ef443abd1b8cdb70bedb88aee29d26a5859a6ed9
ruddus7207 <kimgy05221@daum.net>
2022-09-26 00:53:33 +0900

    photon
    포톤 연결+닉네임+카메라

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public InputField NickNameInput;
+    public GameObject DisconnectPanel;
+    public GameObject MainPanel;
+
+
+    void Awake()
+    {
+        Screen.SetResolution(1920, 1080, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 8 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        DisconnectPanel.SetActive(false);
+        //StartCoroutine("DestroyBullet");
+        Spawn();
+    }
+
+    
+    public void Spawn()
+    {
+        PhotonNetwork.Instantiate("Player", new Vector3(Random.Range(-6f, 19f), 4, 0), Quaternion.identity);
+        MainPanel.SetActive(true);
+    }
+    
+
+    void Update() { if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected) PhotonNetwork.Disconnect(); }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        DisconnectPanel.SetActive(true);
+        MainPanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+using Cinemachine;
+
+public class Player : MonoBehaviourPunCallbacks
+{
+    public PhotonView PV;
+    public Text NickNameText;
+    public float speed = 5;
+
+    bool isGround;
+    Vector3 curPos;
+
+    void Awake()
+    {
+        // 닉네임
+        NickNameText.text = PV.IsMine ? PhotonNetwork.NickName : PV.Owner.NickName;
+        NickNameText.color = PV.IsMine ? Color.yellow : Color.red;
+
+        if (PV.IsMine)
+        {
+            // 2D 카메라
+            var CM = GameObject.Find("CMCamera").GetComponent<CinemachineVirtualCamera>();
+            CM.Follow = transform;
+            CM.LookAt = transform;
+        }
+    }
+
+    void Update()
+    {
+        if (PV.IsMine)
+        {
+            float posX = Input.GetAxisRaw("Horizontal");    //keybord input
+            float posY = Input.GetAxisRaw("Vertical");         //keybord input
+
+            transform.Translate(new Vector2(posX, posY) * Time.deltaTime * speed);
+        }
+        // IsMine이 아닌 것들은 부드럽게 위치 동기화
+        else if ((transform.position - curPos).sqrMagnitude >= 100) transform.position = curPos;
+        else transform.position = Vector3.Lerp(transform.position, curPos, Time.deltaTime * 10);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "AnonymousChatZone")
+        {
+            Debug.Log("익명채팅구역에 들어감");
+            SpriteRenderer spriteR = gameObject.GetComponent<SpriteRenderer>();
+            Sprite[] sprites = Resources.LoadAll<Sprite>("images/ghost");
+            spriteR.sprite = sprites[0];
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.name == "AnonymousChatZone")
+        {
+            Debug.Log("익명채팅구역에서 탈출");
+            SpriteRenderer spriteR = gameObject.GetComponent<SpriteRenderer>();
+            Sprite[] sprites = Resources.LoadAll<Sprite>("images/player");
+            spriteR.sprite = sprites[0];
+        }
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            //stream.SendNext(HealthImage.fillAmount);
+        }
+        else
+        {
+            curPos = (Vector3)stream.ReceiveNext();
+            //HealthImage.fillAmount = (float)stream.ReceiveNext();
+        }
+    }
+
+}
+
+
