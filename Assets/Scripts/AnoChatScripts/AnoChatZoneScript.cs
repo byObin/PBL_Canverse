879e1eb09be5b1780b55484043598c56f5519170
ruddus7207 <kimgy05221@daum.net>
2022-10-10 17:59:38 +0900

    anoChat 1
    익명존 수정1 - 익명존 들어오고 나갈때 채팅 ui 변경됨(들어오면 전체채팅 ui가 꺼지고 익명채팅 ui가 켜지는 방법)

--- Assets/Scripts/AnoChatScripts/AnoChatZoneScript.cs
+++ Assets/Scripts/AnoChatScripts/AnoChatZoneScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AnoChatZoneScript : MonoBehaviour
 {
     public GameObject darkPanel;
+    public GameObject ChattingUI;
+    public GameObject AnoChattingUI;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
@@ -12,7 +14,9 @@ public class AnoChatZoneScript : MonoBehaviour
         {
             Debug.Log("플레이어가 익명채팅구역에 들어옴");
             darkPanel.SetActive(true);
-           
+
+            ChattingUI.SetActive(false);    //전체채팅 ui 끔
+            AnoChattingUI.SetActive(true);    //익명채팅 ui 켬
         }
     }
 
@@ -24,6 +28,8 @@ public class AnoChatZoneScript : MonoBehaviour
             Debug.Log("플레이어가 익명채팅구역에서 나감");
             darkPanel.SetActive(false);
 
+            ChattingUI.SetActive(true);    //전체채팅 ui 켬
+            AnoChattingUI.SetActive(false);    //익명채팅 ui 끔
         }
     }
 
