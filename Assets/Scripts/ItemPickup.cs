8c680cbec4c77470de146fa51be5f2b9ea3988e1
ruddus7207 <kimgy05221@daum.net>
2022-12-09 09:40:07 +0900

    1

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -12,7 +12,8 @@ public class ItemPickup : MonoBehaviour {
     {
         // 원래 Z키 누르면 실행되게 했음.
         // 주웠을 때 소리나게도 하고 ..
-        Inventory.instance.GetAnItem(itemID, _count); // 인벤토리에 추가
+        
+        //Inventory.instance.GetAnItem(itemID, _count); // 인벤토리에 추가
         Destroy(this.gameObject); // 이 gem object 없애버리기
 
 
