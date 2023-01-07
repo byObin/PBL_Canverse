cb5a2031b3933bf95b89cf77e9c6516a0dd2f199
byObin <96936141+byObin@users.noreply.github.com>
2023-01-07 21:18:13 +0900

    스토리 게임 동네 구성
    건물 문에 플레이어 충돌 시 해당 Scene으로 이동

--- Assets/Scripts/StoryGameScripts/Player/Story_Player.cs
+++ Assets/Scripts/StoryGameScripts/Player/Story_Player.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Story_Player : MonoBehaviour
 {
-   
-
-  
 
     public float speed = 5;
 
-
-    void Update()
+    void Update()   // 키보드 방향키에 따라 플레이어 이동
     {
         float posX = Input.GetAxisRaw("Horizontal");    //keybord input
         float posY = Input.GetAxisRaw("Vertical");         //keybord input
@@ -19,13 +16,41 @@ public class Story_Player : MonoBehaviour
 
         transform.Translate(new Vector2(posX, posY) * Time.deltaTime * speed);
 
-
     }
 
 
-    // Start is called before the first frame update
-    void Start()
+    //충돌처리 : 플레이어가 건물 문과 충돌 시
+    void OnTriggerEnter2D(Collider2D collider)
     {
 
+        //도서관 문 충돌 시 Library Scene으로 이동
+        if (collider.gameObject.name == "LibDoor")
+        {
+             Debug.Log("도서관 문에 충돌");
+             SceneManager.LoadScene("Library");
+        }
+
+        //박물관 문 충돌 시 Museum Scene으로 이동
+        if (collider.gameObject.name == "MusDoor")
+        {
+            Debug.Log("박물관 문에 충돌");
+            SceneManager.LoadScene("Museum");
+        }
+
+        //동사무소 문 충돌 시 TownOffice Scene으로 이동
+        if (collider.gameObject.name == "OffiDoor")
+        {
+            Debug.Log("동사무소 문에 충돌");
+            SceneManager.LoadScene("TownOffice");
+        }
+
+        //친구 집 문 충돌 시 Friend'sHouse Scene으로 이동
+        if (collider.gameObject.name == "FirDoor")
+        {
+            Debug.Log("친구 집 문에 충돌");
+            SceneManager.LoadScene("Friend'sHouse");
+        }
+
     }
+
 }
