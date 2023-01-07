f1f1ad30dde9930483e030109b47f8328b55f660
byObin <96936141+byObin@users.noreply.github.com>
2023-01-07 19:38:20 +0900

    스토리 게임 이동버튼
    게임존 옆방 빨간 네모 충돌 인식
    각 구역 별 다크패널 DarkPanel오브젝트로 묶음

--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -13,19 +13,19 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
     public PhotonView PV;
     public Text NickNameText;
 
-    [SerializeField][Range(1f, 10f)] float moveSpeed = 10f; // Inspector에서 스피드 조절 가능
+    [SerializeField] [Range(1f, 10f)] float moveSpeed = 10f; // Inspector에서 스피드 조절 가능
     SpriteRenderer rend; // 이미지 좌우반전용 변수 rend
 
     //bool isGround;
     Vector3 curPos;
 
-    public bool isInAnoZone=false;
+    public bool isInAnoZone = false;
     public Transform darkPanel;
     public int OXGameCount = 0;
 
     public BoxCollider2D boxCollider; // 음 ..
     public Animator animator; // 음 ..
-    
+
     /*void Start()
     {
         if (instance == null)
@@ -53,8 +53,8 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
             var CM = GameObject.Find("CMCamera").GetComponent<CinemachineVirtualCamera>();
             CM.Follow = transform;
             CM.LookAt = transform;
-            
-            
+
+
             //Vector3 pos = CM.transform.position);
             //if (pos.x < 94.84781f) pos.x = 94.84781f;
             /*if (pos.x > 1f) pos.x = 1f;
@@ -84,7 +84,7 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
     */
 
     // 좌우반전 적용 후
-    void Update() 
+    void Update()
     {
         Vector3 flipMove = Vector3.zero; // Vector3.zero는 new Vector(0, 0, 0)과 동일
 
@@ -112,7 +112,7 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         else transform.position = Vector3.Lerp(transform.position, curPos, Time.deltaTime * 10);
     }
 
-    //충돌처리
+    //충돌처리 : 플레이어가 특정 구역 입장 시
     void OnTriggerEnter2D(Collider2D collider)
     {
         //익명존 입장 시 닉네임 USER로 변경, 유령 모습으로 바뀜
@@ -122,20 +122,20 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
             {
                 PV.RPC("AnoRPC", RpcTarget.All);
 
-                GameObject.Find("MainPanel").transform.Find("Else_AnoChatZone").gameObject.SetActive(true); //다크패널 활성화
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_AnoChatZone").gameObject.SetActive(true); //다크패널 활성화
 
                 GameObject.Find("MainPanel").transform.Find("ChattingUI").gameObject.SetActive(false); //전체채팅 ui 비활성화
                 GameObject.Find("MainPanel").transform.Find("AnoChattingUI").gameObject.SetActive(true); //익명채팅 ui 활성화
             }
         }
 
-        //게임존 -> 다크패널
+        //게임존 입장 시 다크패널 활성화
         if (collider.gameObject.name == "GameZone")
         {
             if (PV.IsMine)
             {
                 Debug.Log("플레이어가 게임구역에 들어옴");
-                GameObject.Find("MainPanel").transform.Find("Else_GameZone").gameObject.SetActive(true); //다크패널 활성화
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_GameZone").gameObject.SetActive(true); //다크패널 활성화
             }
         }
 
@@ -157,35 +157,48 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
                 */
             }
 
-            if(OXGameCount >= 2)
+            if (OXGameCount >= 2)
             {
                 PhotonNetwork.LoadLevel("OXGameScene");
             }
         }
 
 
-        //공부존 -> 다크패널
+        //공부존 입장 시 다크패널 활성화
         if (collider.gameObject.name == "StudyZone")
         {
             if (PV.IsMine)
             {
                 Debug.Log("플레이어가 공부구역에 들어옴");
-                GameObject.Find("MainPanel").transform.Find("Else_StudyZone").gameObject.SetActive(true); //다크패널 활성화
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_StudyZone").gameObject.SetActive(true); //다크패널 활성화
             }
         }
 
-        //상점 -> 다크패널
+        //상점입장 시 다크패널 활성화
         if (collider.gameObject.name == "StoreZone")
         {
             if (PV.IsMine)
             {
                 Debug.Log("플레이어가 상점에 들어옴");
-                GameObject.Find("MainPanel").transform.Find("Else_StoreZone").gameObject.SetActive(true); //다크패널 활성화
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_StoreZone").gameObject.SetActive(true); //다크패널 활성화
             }
         }
+
+
+        //스토리 이동 버튼과 충돌 시 다크패널 활성화
+        if (collider.gameObject.name == "MoveToStory")
+        {
+            if (PV.IsMine)
+            {
+                Debug.Log("스토리 이동 버튼과 충돌");
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_MoveToStoryZone").gameObject.SetActive(true); //다크패널 활성화
+            }
+        }
+
+       
     }
 
-    //충돌처리
+    //충돌처리 : 플레이어가 특정 구역 탈출 시
     void OnTriggerExit2D(Collider2D collider)
     {
         //익명존 탈출 시 원상복구
@@ -195,7 +208,7 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
             {
                 PV.RPC("AnoExitRPC", RpcTarget.All);
 
-                GameObject.Find("MainPanel").transform.Find("Else_AnoChatZone").gameObject.SetActive(false); //다크패널 비활성화
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_AnoChatZone").gameObject.SetActive(false); //다크패널 비활성화
 
                 GameObject.Find("MainPanel").transform.Find("ChattingUI").gameObject.SetActive(true); //전체채팅 ui 활성화
                 GameObject.Find("MainPanel").transform.Find("AnoChattingUI").gameObject.SetActive(false); //익명채팅 ui 비활성화
@@ -208,13 +221,13 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
             if (PV.IsMine)
             {
                 Debug.Log("플레이어가 게임구역에서 나감");
-                GameObject.Find("MainPanel").transform.Find("Else_GameZone").gameObject.SetActive(false); //다크패널 비활성화
-                
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_GameZone").gameObject.SetActive(false); //다크패널 비활성화
+
                 //GameObject.Find("MainPanel").transform.Find("OXGameUI").gameObject.SetActive(false); //OXGame UI 비활성화
-               
+
                 GameObject.Find("MainPanel").transform.Find("ButtonUI").gameObject.SetActive(true); //버튼 ui 활성화
                 GameObject.Find("MainPanel").transform.Find("ChattingUI").gameObject.SetActive(true); //채팅 ui 활성화
-                
+
             }
         }
 
@@ -226,7 +239,7 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
                 OXGameCount--;
 
                 Debug.Log("플레이어가 OX 게임구역을 나감");
-                
+
 
                 GameObject.Find("MainPanel").transform.Find("ChattingUI").gameObject.SetActive(true); //전체채팅 ui 활성화
                 GameObject.Find("MainPanel").transform.Find("ButtonUI").gameObject.SetActive(true); //버튼 ui 활성화
@@ -240,7 +253,7 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
             if (PV.IsMine)
             {
                 Debug.Log("플레이어가 공부 구역에서 나감");
-                GameObject.Find("MainPanel").transform.Find("Else_StudyZone").gameObject.SetActive(false); //다크패널 비활성화
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_StudyZone").gameObject.SetActive(false); //다크패널 비활성화
             }
         }
 
@@ -250,12 +263,24 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
             if (PV.IsMine)
             {
                 Debug.Log("플레이어가 상점 구역에서 나감");
-                GameObject.Find("MainPanel").transform.Find("Else_StoreZone").gameObject.SetActive(false); //다크패널 비활성화
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_StoreZone").gameObject.SetActive(false); //다크패널 비활성화
             }
         }
+
+
+        //스토리 이동 버튼 벗어나면 다크패널 비활성화
+        if (collider.gameObject.name == "MoveToStory")
+        {
+            if (PV.IsMine)
+            {
+                Debug.Log("스토리 이동 버튼에서 떨어짐");
+                GameObject.Find("MainPanel").transform.Find("DarkPanel").transform.Find("Else_MoveToStoryZone").gameObject.SetActive(false); //다크패널 비활성화
+            }
+        }
+
     }
 
-    [PunRPC] 
+    [PunRPC]
     void AnoRPC()
     {
         Debug.Log("익명채팅구역에 들어감");
@@ -278,7 +303,7 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if (stream.IsWriting)   
+        if (stream.IsWriting)
         {
             stream.SendNext(transform.position);
         }
@@ -287,6 +312,12 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
             curPos = (Vector3)stream.ReceiveNext();
         }
     }
+
+
+  
+        
+
+  
 
 }
 
