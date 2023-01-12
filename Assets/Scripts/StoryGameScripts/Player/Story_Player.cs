8d120a91134c6a47893d3a09fd076ea03806dafe
byObin <96936141+byObin@users.noreply.github.com>
2023-01-12 15:41:19 +0900

    village 나무밑 npc 대화
    npc 클릭 시 대화창,
    ok 버튼 클릭 시 대화창 꺼짐

--- Assets/Scripts/StoryGameScripts/Player/Story_Player.cs
+++ Assets/Scripts/StoryGameScripts/Player/Story_Player.cs
@@ -53,4 +53,5 @@ public class Story_Player : MonoBehaviour
 
     }
 
+
 }
---
+++ Assets/Scripts/StoryGameScripts/UnderTreeNPC.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnderTreeNPC : MonoBehaviour
+{
+    public GameObject npcChatPanel;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("npc is clicked");
+            npcChatPanel.SetActive(true);
+            
+        }
+    }
+
+    void buttonclick()
+    {
+        npcChatPanel.SetActive(false);
+    }
+}
