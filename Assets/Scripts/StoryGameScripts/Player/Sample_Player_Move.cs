8194b8a21773776f7c659cdecc4db63cb3c97506
byObin <96936141+byObin@users.noreply.github.com>
2023-01-16 20:39:13 +0900

    도서관 책사이 지도
    책장 빨간 직사각형 클릭하면 지도 뜸

--- Assets/Scripts/StoryGameScripts/Player/Sample_Player_Move.cs
+++ Assets/Scripts/StoryGameScripts/Player/Sample_Player_Move.cs
@@ -6,6 +6,8 @@ public class Sample_Player_Move : MonoBehaviour
 {
     public float moveSpeed = 5;
     private Animator anim;
+    public GameObject mapPanel;
+
 
     // Start is called before the first frame update
     void Start()
@@ -16,12 +18,12 @@ public class Sample_Player_Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetAxisRaw("Horizontal")> 0f || Input.GetAxisRaw("Horizontal") < 0f)
+        if (Input.GetAxisRaw("Horizontal") > 0f || Input.GetAxisRaw("Horizontal") < 0f)
         {
             transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
         }
 
-        if(Input.GetAxisRaw("Vertical")>0f || Input.GetAxisRaw("Vertical") < 0f)
+        if (Input.GetAxisRaw("Vertical") > 0f || Input.GetAxisRaw("Vertical") < 0f)
         {
             transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
         }
@@ -29,5 +31,23 @@ public class Sample_Player_Move : MonoBehaviour
         anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
         anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
 
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hitInformation = Physics2D.Raycast(touchPos, Camera.main.transform.forward);
+            if (hitInformation.collider != null)
+            {
+                //We should have hit something with a 2D Physics collider!
+                GameObject touchedObject = hitInformation.transform.gameObject;
+                //touchedObject should be the object someone touched.
+                Debug.Log("touched Object is " + touchedObject);
+                if (touchedObject.name == "MapItem")
+                {
+                    mapPanel.SetActive(true);
+                }
+            }
+
+        }
+    
     }
 }
