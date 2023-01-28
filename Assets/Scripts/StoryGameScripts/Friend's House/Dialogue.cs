b775bbdecfded5b19bc40f641a489e1dc3b252c2
ukkiyeon <ukkiyeon@naver.com>
2023-01-29 05:20:55 +0900

    Friend's House Dialogue1 수정

--- Assets/Scripts/StoryGameScripts/Friend's House/Dialogue.cs
+++ Assets/Scripts/StoryGameScripts/Friend's House/Dialogue.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue // 커스텀 클래스
 {
-    [TextArea(1, 2)]
     public string[] sentences; // 문장을 배열로 만듦. 여러 문장이니까.
-    public Sprite[] sprites; // 우린 필요 없을 듯
+
+    /* 우린 필요 없을 듯
+    public Sprite[] sprites; 
     public Sprite[] dialogueWindows;
-    
+    */
 }
